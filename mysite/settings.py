d2eae86e903988f2f7fb572e410f6bbd3e814b66
skarp <s-karp@wp.pl>
2022-08-07 22:22:18 +0200

    from_pycharm2507

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,6 +146,8 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
+CSRF_TRUSTED_ORIGINS = ['https://btmonika.pl']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
