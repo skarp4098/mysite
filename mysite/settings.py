a0f355a2e33af83e46e657d620968db182eb4765
skarp <s-karp@wp.pl>
2022-08-23 16:43:24 +0200

    from_pycharm2507

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,8 +26,8 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!#
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 
 
